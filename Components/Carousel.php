23e67493e9d6802c62157afe08584a429a4b2447
Anuwat2542 <tunwaking2542@gmail.com>
2024-04-09 16:30:15 +0700

    index

--- Components/Carousel.php
+++ Components/Carousel.php
@@ -1,20 +1,20 @@
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="Img/1.png" class="d-block w-100 " height="670px" alt="..." >
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="img/2.jpg"  class="img-fluid" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="img/2.jpg"  class="img-fluid" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="Img/2.jpg" class="d-block w-100"  height="670px" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="Img/3.jpg" class="d-block w-100"  height="670px" alt="...">
+      <img src="img/2.jpg"  class="img-fluid" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
