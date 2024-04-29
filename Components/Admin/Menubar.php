9d2c595ee6eca831c7dc4c8e24bdb9519c7ee0e5
ArtSubNo1 <Art0934191573@mail.com>
2024-04-29 18:35:48 +0700

    up

--- Components/Admin/Menubar.php
+++ Components/Admin/Menubar.php
@@ -8,72 +8,73 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            width: 320px;
-            background-color: #f1f1f1;
-        }
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 320px;
+        background-color: #f1f1f1;
+    }
 
-        li a {
-            display: block;
-            color: #000;
-            padding: 8px 16px;
-            text-decoration: none;
-        }
+    li a {
+        display: block;
+        color: #000;
+        padding: 8px 16px;
+        text-decoration: none;
+    }
 
-        li a.active {
-            background-color: #3333FF;
-            color: white;
-        }
+    li a.active {
+        background-color: #3333FF;
+        color: white;
+    }
 
-        li a:hover:not(.active) {
-            background-color: #555;
-            color: white;
-        }
+    li a:hover:not(.active) {
+        background-color: #555;
+        color: white;
+    }
 
-        .dropdown-menu {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 1;
-        }
+    .dropdown-menu {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
 
-        .dropdown-menu a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
+    .dropdown-menu a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
 
-        .dropdown-menu a:hover {
-            background-color: #f1f1f1;
-        }
+    .dropdown-menu a:hover {
+        background-color: #f1f1f1;
+    }
 
-        .dropdown:hover .dropdown-menu {
-            display: block;
-        }
+    .dropdown:hover .dropdown-menu {
+        display: block;
+    }
     </style>
 </head>
+
 <body>
     <h4>การจัดการข้อมูล</h4>
     <ul>
-        <li><a class="active" href="../admin_dashboard.php">Home</a></li>
+        <li><a class="active" href="Admin/admin_dashboard.php">Home</a></li>
         <li><a href="../Manage/manage_member.php">จัดการรายชื่อAdmin</a></li>
         <li><a href="#about">จัดการสถานะ</a></li>
         <li><a href="#about">จัดการระดับชั้น</a></li>
         <li><a href="../Manage/manage_branches.php">จัดการสาขาที่อยู่ในคณะวิศวกรรมศาสตร์</a></li>
-        <li><a href="./manage_attention.php">จัดการรายชื่อความสนใจ</a></li>
+        <li><a href="../Manage/manage_attention.php">จัดการรายชื่อความสนใจ</a></li>
         <li><a href="#about">จัดการค่าความสนใจในเเต่ละวิชา</a></li>
         <li class="dropdown">
             <a href="#" class="dropdown-toggle">กำหนดค่าความสำคัญของวิชาในเเต่ละสาขา</a>
             <ul class="dropdown-menu">
-                <li><a href="./manage_importance_subject6.php">ม.6</a></li>
-                <li><a href="./manage_importance_subject3.php">ปวช.</a></li>
-                <li><a href="./manage_importance_subject2.php">ปวส.</a></li>
+                <li><a href="../Manage/manage_importance_subject6.php">ม.6</a></li>
+                <li><a href="../Manage/manage_importance_subject3.php">ปวช.</a></li>
+                <li><a href="../Manage/manage_importance_subject2.php">ปวส.</a></li>
             </ul>
         </li>
     </ul>
--- form1/formgrade12.php
+++ form1/formgrade12.php
@@ -10,9 +10,16 @@
 
 <body>
     <?php
-    include('../Components/navbar.php');
+    include('../Components/Navbar.php');
     include('../Connent/connent.php');
-
+    $total_membership_grade = 0;
+    $total_membership_interest = 0;
+    // ดึงข้อมูลรายวิชา
+    $sql_grade = "SELECT * FROM subjects";
+    $result_grade = $connect->query($sql_grade);
+    // ดึงข้อมูลความสนใจ
+    $sql_interest = "SELECT * FROM attention";
+    $result_interest = $connect->query($sql_interest);
     function right_shoulder($x, $a, $b)
     {
         if ($x <= $a) {
@@ -23,103 +30,91 @@
             return ($x - $a) / ($b - $a);
         }
     }
-
     $a = 0;
     $b = 4;
-
-    // ดึงข้อมูลรายวิชา
-    $sql_subject = "SELECT * FROM subjects WHERE  study_level_Id ='1'";
-    $result_subject = $connect->query($sql_subject);
-
-    // เก็บข้อมูลรายวิชา
-    $data = [];
-    while ($row_subject = $result_subject->fetch_assoc()) {
-        $data[] = $row_subject;
-    }
-
     // ตรวจสอบว่ามีการส่งข้อมูลแบบ POST มาหรือไม่
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // เก็บข้อมูลที่รับมาจากฟอร์ม
         $grades = isset($_POST['grades']) ? $_POST['grades'] : [];
-
-        // เก็บค่าความเป็นสมาชิกของแต่ละวิชา
-        $membershipValues = [];
-        foreach ($grades as $subjectId => $grade) {
-            // คำนวณค่าความเป็นสมาชิก
-            $membership_value = right_shoulder(floatval($grade), $a, $b);
-
-            // เก็บค่าความเป็นสมาชิก
-            $membershipValues[$subjectId] = $membership_value;
-        }
-
-        // เรียงลำดับค่าความเป็นสมาชิกจากรายวิชาที่มีค่ามากที่สุดไปน้อยที่สุด
-        arsort($membershipValues);
+        $scores = isset($_POST['scores']) ? $_POST['scores'] : [];
     }
     ?>
     <div class="container">
         <br>
-        <h3>รายวิชาพื้นฐานระดับมัธยมศึกษาปีที่ 6</h3>
-        <div class="container">
-            <div class="row">
-                <div class="col-6">
-                    <form method="post">
-                        <table class="table">
-                            <thead>
+        <div class="row">
+            <div class="col">
+                <h3>รายวิชาพื้นฐานระดับมัธยมศึกษาปีที่ 6</h3>
+                <form method="post">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ชื่อรายวิชา</th>
+                                <th scope="col">เกรดเเต่ละรายวิชา</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while ($row = $result_grade->fetch_assoc()) : ?>
                                 <tr>
-                                    <th scope="col">รหัสวิชา</th>
-                                    <th scope="col">ชื่อรายวิชา</th>
-                                    <th scope="col">เกรดเเต่ละรายวิชา</th>
+                                    <td>
+                                        <?php echo $row['subject_name']; ?>
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" name="grades[<?php echo $row['subject_Id']; ?>]" placeholder="กรอกเกรด">
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($data as $row) : ?>
-                                    <tr>
-                                        <td><?php echo $row['subject_Id']; ?></td>
-                                        <td><?php echo $row['subject_name']; ?></td>
-                                        <td>
-                                            <input type="text" class="form-control" name="grades[<?php echo $row['subject_Id']; ?>]" placeholder="กรอกเกรด">
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                        <button type="submit" class="btn btn-success"><i class="fa-solid fa-calculator"></i> คำนวณ</button>
-                    </form>
-                </div>
-                <div class="col">
-                    <?php if ($_SERVER["REQUEST_METHOD"] == "POST") : ?>
-                        <h2>รายวิชาที่มีค่ามากที่สุด 4 ลำดับ:</h2>
-                        <table class='table'>
-                            <thead>
-                                <tr>
-                                    <th scope='col'>รหัสวิชา</th>
-                                    <th scope='col'>ชื่อวิชา</th>
-                                    <th scope='col'>ค่าความเป็นสมาชิก</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $count = 0;
-                                foreach ($membershipValues as $subjectId => $membershipValue) {
-                                    $subjectname = $data[$subjectId - 1]['subject_name']; ?>
-                                    <tr>
-                                        <td><?php echo $subjectId; ?></td>
-                                        <td><?php echo $subjectname; ?></td>
-                                        <td><?php echo number_format($membershipValue, 2); ?></td>
-                                    </tr>
-                                <?php
-                                    $count++;
-                                    if ($count >= 4) {
-                                        break;
-                                    }
-                                } ?>
-                            </tbody>
-                        </table>
-                    <?php endif; ?>
-                </div>
+                            <?php endwhile ?>
+                        </tbody>
+                    </table>
+            </div>
+            <div class="col">
+                <h3>ด้านความสนใจระดับมัธยมศึกษาปีที่ 6</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ชื่อรายวิชา</th>
+                            <th scope="col">คะเเนนความสนใจเเต่ละรายวิชา</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($row = $result_interest->fetch_assoc()) : ?>
+                            <tr>
+                                <td>
+                                    <?php echo $row['attention_name']; ?>
+                                </td>
+                                <td>
+                                    <input type="radio" name="scores[<?php echo $row['attention_Id']; ?>]" value="4"> ดีมาก
+                                    <input type="radio" name="scores[<?php echo $row['attention_Id']; ?>]" value="3"> ดี
+                                    <input type="radio" name="scores[<?php echo $row['attention_Id']; ?>]" value="2"> พอให้
+                                    <input type="radio" name="scores[<?php echo $row['attention_Id']; ?>]" value="1"> ปรับปรุง
+                                </td>
+                            </tr>
+                        <?php endwhile ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-success" onclick="calculate()"><i class="fa-solid fa-calculator"></i> คำนวณ</button>
+        </form>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && (empty($grades) || empty($scores))) : ?>
+                    <h2>กรุณาเพิ่มข้อมูล</h2>
+                <?php else : ?>
+                    <h2>ผลลัพธ์</h2>
+                    <h3>ค่าความเป็นสมาชิกทั้งหมด: <?php echo  number_format($total_membership_grade + $total_membership_interest, 2)  ?> </h3>
+                <?php endif; ?>
             </div>
         </div>
     </div>
+    <script>
+        function calculate() {
+            // รีโหลดหน้าหลังจากคำนวณ
+            document.getElementById("result-container").innerHTML = "";
+            location.reload();
+        }
+    </script>
     <script src="https://kit.fontawesome.com/58d7e3d562.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
